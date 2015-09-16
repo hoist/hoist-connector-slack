85fe87155136a7884f0778094b86dd4af735e04a
Owen Evans <owen@bgeek.net>
2015-09-16 21:46:56 +1200

    use base-box

--- deploy.sh
+++ deploy.sh
@@ -6,22 +6,22 @@ TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 
 echo "making timestamped deploy directory";
 
-mkdir -p ${ROOT_CONNECTOR_DIR}/hoist-connector-slack/${TIMESTAMP}
+mkdir -p ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/${TIMESTAMP}
 
 echo "copying files to deploy directory";
 
-cp -r . ${ROOT_CONNECTOR_DIR}/hoist-connector-slack/${TIMESTAMP}
+cp -r . ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/${TIMESTAMP}
 
 echo "removing current symlink";
 
-rm -f ${ROOT_CONNECTOR_DIR}/hoist-connector-slack/current
+rm -f ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/current
 
 echo "relinking current symlink";
 
-ln -s ${ROOT_CONNECTOR_DIR}/hoist-connector-slack/${TIMESTAMP} ${ROOT_CONNECTOR_DIR}/hoist-connector-slack/current
+ln -s ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/${TIMESTAMP} ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/current
 
 echo "removing old deploy directories";
 
-(ls -t ${ROOT_CONNECTOR_DIR}/hoist-connector-slack/|head -n 5;ls ${ROOT_CONNECTOR_DIR}/hoist-connector-slack/)|sort|uniq -u|xargs -I '{}' rm -r ${ROOT_CONNECTOR_DIR}/hoist-connector-slack/'{}'
+(ls -t ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/|head -n 5;ls ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/)|sort|uniq -u|xargs -I '{}' rm -r ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/'{}'
 
 echo "done!";
